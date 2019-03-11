7d46689c15b8d1dfa3613f02a88146c0f61744bb
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-12 00:44:03 +0300

    lab3 complete

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,16 +1,18 @@
 # Emelyanov SI Lab3
 
-# Exercise
+# 8. Пользователь вводит две квадратные матрицы, посчитать абсолютную величину разности из определителей.
 
 import numpy as np
 
-arr = []
-print("\nInput array:")
-for i in range(3):
-    arr.append([int(j) for j in input().split()])
-arr = np.array(arr)
-arr = arr * arr.transpose()
-print("Result:")
-print(arr)
-
+n = int(input("Input n: "))
+arr1 = []
+arr2 = []
+print("Input first array:")
+for i in range(n):
+    arr1.append([int(j) for j in input().split()])
+print("Input second array:")
+for i in range(n):
+    arr2.append([int(j) for j in input().split()])
+res = abs(np.linalg.det(arr1) - np.linalg.det(arr2))
+print("Result: " + str(int(res)))
 # Lab3 end
