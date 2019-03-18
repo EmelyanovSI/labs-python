021b3696d1b3fffac1ab53d16bb76d703dc1a19e
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-18 11:07:53 +0300

    fixed

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -9,7 +9,7 @@ print("\nInput array:")
 for i in range(3):
     arr.append([int(j) for j in input().split()])
 arr = np.array(arr)
-arr = arr * arr.transpose()
+arr = arr.transpose() * arr  # было - arr = arr * arr.transpose()
 print("Result:")
 print(arr)
 # Lab2 end
